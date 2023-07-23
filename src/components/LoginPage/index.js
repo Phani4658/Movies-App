a6a1459bc76507fcbe73964f49c3aa7ccd4647c2
Phani <mysteriousphani@gmail.com>
2023-07-23 09:08:02 +0000

    Authentication logic

--- src/components/LoginPage/index.js
+++ src/components/LoginPage/index.js
@@ -1,42 +1,79 @@
 import './index.css'
 import {useState} from 'react'
+import Cookies from 'js-cookie'
+import {Redirect} from 'react-router-dom'
 
-const LoginPage = () => {
-  const login = 'Hello'
+const LoginPage = props => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [isSuccessful, setIsSuccessful] = useState(true)
+  const [errorMsg, setErrorMsg] = useState('')
 
-  const loginPageUserNameField = () => {
-    console.log('Username')
-    return (
-      <div className="form-field-container">
-        <label htmlFor="username" className="field-label">
-          Username
-        </label>
-        <input
-          type="text"
-          id="username"
-          className="form-field"
-          placeholder="Enter your Username"
-        />
-      </div>
-    )
+  if (Cookies.get('jwt_token') !== undefined) {
+    return <Redirect to="/" />
   }
 
-  const loginPagePasswordField = () => {
-    console.log('Username')
-    return (
-      <div className="form-field-container" style={{marginBottom: '10px'}}>
-        <label htmlFor="password" className="field-label">
-          Password
-        </label>
-        <input
-          type="text"
-          id="password"
-          className="form-field"
-          placeholder="Enter your Password"
-        />
-      </div>
-    )
+  const onChangeUsernameField = event => {
+    setUsername(event.target.value)
   }
+
+  const onChangePasswordField = event => {
+    setPassword(event.target.value)
+  }
+
+  const onClickLoginButton = async event => {
+    event.preventDefault()
+    const {history} = props
+    const userDetails = {username, password}
+    const options = {
+      method: 'POST',
+      body: JSON.stringify(userDetails),
+    }
+    const apiUrl = 'https://apis.ccbp.in/login'
+
+    const apiResult = await fetch(apiUrl, options)
+    const result = await apiResult.json()
+    if (apiResult.ok) {
+      Cookies.set('jwt_token', result.jwt_token, {expires: 7})
+      history.replace('/')
+    } else {
+      setErrorMsg(result.error_msg)
+      setIsSuccessful(false)
+    }
+
+    setUsername('')
+    setPassword('')
+  }
+
+  const loginPageUserNameField = () => (
+    <div className="form-field-container">
+      <label htmlFor="username" className="field-label">
+        Username
+      </label>
+      <input
+        type="text"
+        id="username"
+        className="form-field"
+        placeholder="Enter your Username"
+        onChange={onChangeUsernameField}
+      />
+    </div>
+  )
+
+  const loginPagePasswordField = () => (
+    <div className="form-field-container" style={{marginBottom: '10px'}}>
+      <label htmlFor="password" className="field-label">
+        Password
+      </label>
+      <input
+        type="password"
+        id="password"
+        className="form-field"
+        placeholder="Enter your Password"
+        onChange={onChangePasswordField}
+      />
+    </div>
+  )
 
   return (
     <section id="loginSection">
@@ -50,8 +87,8 @@ const LoginPage = () => {
         <h1 className="login-form-heading">Login</h1>
         {loginPageUserNameField()}
         {loginPagePasswordField()}
-        <p className="error-msg">error Msg</p>
-        <button id="signInBtn" type="submit">
+        {!isSuccessful && <p className="error-msg">{errorMsg}</p>}
+        <button id="signInBtn" type="submit" onClick={onClickLoginButton}>
           Sign in
         </button>
       </form>
